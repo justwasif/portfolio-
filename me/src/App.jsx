3fc9aac3bda23e9d1d2f156d5f2586c5f7ad395c
just_wasif <wasif.iitr.ansari@gmail.com>
2025-09-30 05:05:30 +0530

    STARTING:

--- me/src/App.jsx
+++ me/src/App.jsx
@@ -1,15 +1,14 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from "./component/Header";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div className="flex items-start h-64 bg-gray-200">Hello World</div>
     </>
   )
 }
