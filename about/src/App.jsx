cb87e80e79563a9fd82e4d6ffb9bda66bd1a2448
justwasif <wasif.iit.ansari@gmail.com>
2025-09-29 08:27:30 +0530

    learning react dom

---
+++ about/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from "./component/Header";
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Header/>
+    
+
+      <div className='bg-white flex-wrap ' >
+        <h1>me</h1>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
